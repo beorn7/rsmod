6d36d15062f7eea279bc44f10f30012979dafec4
Björn Rabenstein <bjoern@rabenste.in>
2013-09-26 15:53:31 +0200

    added action.py and game.py

---
+++ action.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""Module containing the action class hierarchy.
+
+TODO: Explain how actions work in general.
+"""
+
+class Action(object):
+    pass
